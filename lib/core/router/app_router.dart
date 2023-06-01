95cfb7e4c294dbf1f6ed6e632af3581f8fee1c53
Yuzucchi-cist <yuzucchi.cist@gmail.com>
2023-06-01 18:39:58 +0900

    #3 [add] ログイン画面作成

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 
+import '../../feature/auth/presentation/pages/login_page.dart';
 import '../../feature/home/presentation/pages/home_page.dart';
 
 part 'app_router.gr.dart';
@@ -9,5 +10,6 @@ class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(path: '/', page: HomeRoute.page),
+        AutoRoute(path: '/login', page: LoginRoute.page),
       ];
 }
