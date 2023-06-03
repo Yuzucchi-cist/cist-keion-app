e7b11a3882f3a94bf2ee00d7a687becd9272dcef
Yuzucchi-cist <yuzucchi.cist@gmail.com>
2023-06-03 21:18:42 +0900

    #4 [add] 予約完了画面作成

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -7,6 +7,7 @@ import '../../feature/home/presentation/pages/home_page.dart';
 import '../../feature/reservation/presentation/pages/choose_reserve_table_page.dart';
 import '../../feature/reservation/presentation/pages/confirm_reservation_page.dart';
 import '../../feature/reservation/presentation/pages/make_reservation_detail_page.dart';
+import '../../feature/reservation/presentation/pages/reservation_complete_page.dart';
 
 part 'app_router.gr.dart';
 
@@ -23,5 +24,7 @@ class AppRouter extends _$AppRouter {
             path: '/reserveDetail', page: MakeReservationDetailRoute.page),
         AutoRoute(
             path: '/confirm_reservation', page: ConfirmReservationRoute.page),
+        AutoRoute(
+            path: '/reservation_complete', page: ReservationCompleteRoute.page),
       ];
 }
