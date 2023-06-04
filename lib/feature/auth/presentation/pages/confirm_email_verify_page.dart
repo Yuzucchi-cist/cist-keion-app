906fbcb3d8ed48b44b219c19b9921ef78bae476f
Yuzucchi-cist <yuzucchi.cist@gmail.com>
2023-06-04 12:55:30 +0900

    #7 [add] メール認証確認画面の画面遷移実装

--- lib/feature/auth/presentation/pages/confirm_email_verify_page.dart
+++ lib/feature/auth/presentation/pages/confirm_email_verify_page.dart
@@ -1,6 +1,8 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../../core/router/app_router.dart';
 
 @RoutePage()
 class ConfirmEmailVerifyPage extends ConsumerWidget {
@@ -13,9 +15,28 @@ class ConfirmEmailVerifyPage extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('メール認証確認'),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
       ),
-      body: const Text('認証メールを送信しました。\n'
-          '記載されているURLにアクセスして、認証を完了してください。'),
+      body: Column(
+        children: [
+          const Text('認証メールを送信しました。\n'
+              '記載されているURLにアクセスして、認証を完了してください。'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                child: const Text('ログイン'),
+                onPressed: () => context.router.push(const LoginRoute()),
+              ),
+              TextButton(
+                child: const Text('ホームへ'),
+                onPressed: () => context.router.push(const HomeRoute()),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
