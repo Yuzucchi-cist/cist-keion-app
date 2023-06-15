d54576b569e6382be9f3aa738b69077cb0b17270
Yuzucchi-cist <yuzucchi.cist@gmail.com>
2023-06-15 18:42:17 +0900

    #38 [update] コミットを忘れていたファイルの追加

---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,14 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  NetworkInfoImpl(this.connectivity);
+
+  final Connectivity connectivity;
+  @override
+  Future<bool> get isConnected async =>
+      await connectivity.checkConnectivity() != ConnectivityResult.none;
+}
