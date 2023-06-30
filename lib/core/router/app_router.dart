88cedf91b000586488c6156b858b0dd9938d8d84
Yuzucchi-cist <yuzucchi.cist@gmail.com>
2023-07-01 04:54:48 +0900

    #38 [fix] 誤って消していたアノテーションの復元

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -18,6 +18,7 @@ import '../../feature/suggestion/presentation/pages/create_suggestion_page.dart'
 
 part 'app_router.gr.dart';
 
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
